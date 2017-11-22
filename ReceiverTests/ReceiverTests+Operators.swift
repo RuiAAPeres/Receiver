26e45f0788b11d7a4dffe5651a6d6d34afd73072
Rui Peres <sauron.is.alive@gmail.com>
2017-11-22 21:38:53 +0000

    filter 😎

--- ReceiverTests/ReceiverTests+Operators.swift
+++ ReceiverTests/ReceiverTests+Operators.swift
@@ -3,7 +3,7 @@ import XCTest
 
 class ReceiverTests_Operators: XCTestCase {
     
-    func test_Map() {
+    func test_map() {
         let (transmitter, receiver) = Receiver<Int>.make()
         let newReceiver = receiver.map(String.init)
         var called = 0
@@ -16,4 +16,21 @@ class ReceiverTests_Operators: XCTestCase {
         transmitter.broadcast(1)
         XCTAssertTrue(called == 1)
     }
+
+    func test_filter() {
+        let (transmitter, receiver) = Receiver<Int>.make()
+        let newReceiver = receiver.filter { $0 % 2 == 0}
+        var called = 0
+
+        newReceiver.listen { wave in
+            XCTAssertTrue(wave == 2)
+            called = called + 1
+        }
+
+        transmitter.broadcast(1)
+        transmitter.broadcast(2)
+        transmitter.broadcast(3)
+
+        XCTAssertTrue(called == 1)
+
 }
