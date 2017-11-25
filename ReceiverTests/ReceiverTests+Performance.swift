daac33528bdacd77f041d307704a4858f2398fa3
Rui Peres <sauron.is.alive@gmail.com>
2017-11-26 01:22:44 +0200

    Added one performance test

---
+++ ReceiverTests/ReceiverTests+Performance.swift
@@ -0,0 +1,21 @@
+import XCTest
+@testable import Receiver
+
+class ReceiverTests_Performance: XCTestCase {
+
+    func test_map_filter_performance() {
+        // at ~`0.213`
+        self.measure {
+            let (transmitter, receiver) = Receiver<Int>.make()
+            let newReceiver = receiver
+                .map { $0 * 3 }
+                .filter { $0 / 2 > 0}
+                .map { $0 * 3 }
+
+            for i in 1...1000 {
+                newReceiver.listen { _ in }
+                transmitter.broadcast(i)
+            }
+        }
+    }
+}
